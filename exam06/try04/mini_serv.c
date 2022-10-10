e8b73e337fbc67869a1e4e648aeecf7134486843
subin195-09 <sbkim@superb-ai.com>
2022-10-10 19:19:01 +0900

    exam ready

---
+++ exam06/try04/mini_serv.c
@@ -0,0 +1,170 @@
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int extract_message(char **buf, char **msg)
+{
+	char	*newbuf;
+	int	i;
+
+	*msg = 0;
+	if (*buf == 0)
+		return (0);
+	i = 0;
+	while ((*buf)[i])
+	{
+		if ((*buf)[i] == '\n')
+		{
+			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+			if (newbuf == 0)
+				return (-1);
+			strcpy(newbuf, *buf + i + 1);
+			*msg = *buf;
+			(*msg)[i + 1] = 0;
+			*buf = newbuf;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+char *str_join(char *buf, char *add)
+{
+	char	*newbuf;
+	int		len;
+
+	if (buf == 0)
+		len = 0;
+	else
+		len = strlen(buf);
+	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+	if (newbuf == 0)
+		return (0);
+	newbuf[0] = 0;
+	if (buf != 0)
+		strcat(newbuf, buf);
+	free(buf);
+	strcat(newbuf, add);
+	return (newbuf);
+}
+
+int fd_max = 0;
+int clients = 0;
+int idx[65536];
+char *msg[65536];
+char rbuf[1025], wbuf[42];
+fd_set rfds, wfds, fds;
+
+void fatal() {
+	write(2, "Fatal error\n", strlen("Fatal error\n"));
+	exit(1);
+}
+
+void notify(int from, char *s) {
+	for (int fd = 0; fd <= fd_max; fd++) {
+		if (FD_ISSET(fd, &wfds) && from != fd) {
+			send(fd, s, strlen(s), 0);
+		}
+	}
+}
+
+void deliver(int fd) {
+	char *s;
+
+	while (extract_message(&(msg[fd]), &s)) {
+		sprintf(wbuf, "client %d: ", idx[fd]);
+		notify(fd, wbuf);
+		notify(fd, s);
+		free(s);
+		s = NULL;
+	}
+}
+
+int create_client(void) {
+	FD_ZERO(&fds);
+	fd_max = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd_max == -1) {
+		fatal();
+	}
+	FD_SET(fd_max, &fds);
+	return (fd_max);
+}
+
+void add_client(int fd) {
+	fd_max = fd > fd_max ? fd : fd_max;
+
+	idx[fd] = clients++;
+	msg[fd] = NULL;
+	FD_SET(fd, &fds);
+	sprintf(wbuf, "server: client %d just arrived\n", idx[fd]);
+	notify(fd, wbuf);
+}
+
+void remove_client(int fd) {
+	sprintf(wbuf, "server: client %d just left\n", idx[fd]);
+	notify(fd, wbuf);
+	free(msg[fd]);
+	msg[fd] = NULL;
+	FD_CLR(fd, &fds);
+	close(fd);
+}
+
+int main(int ac, char **av) {
+	if (ac != 2) {
+		write(2, "Wrong number of arguments\n", strlen("Wrong number of arguments\n"));
+		exit(1);
+	}
+
+	int sockfd = create_client();
+
+	struct sockaddr_in servaddr;
+	bzero(&servaddr, sizeof(servaddr));
+
+	// assign IP, PORT
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
+	servaddr.sin_port = htons(atoi(av[1]));
+
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
+		fatal();
+	}
+	if (listen(sockfd, 10) != 0) {
+		fatal();
+	}
+
+	while (1) {
+		rfds = wfds = fds;
+		if (select(fd_max + 1, &rfds, &wfds, NULL, NULL) < 0)
+			fatal();
+		for (int fd = 0; fd <= fd_max; fd++) {
+			if (!FD_ISSET(fd, &rfds))
+				continue;
+			if (fd == sockfd) {
+				socklen_t len = sizeof(servaddr);
+				int client = accept(sockfd, (struct sockaddr *)&servaddr, &len);
+				if (client < 0) {
+					fatal();
+				} else {
+					add_client(client);
+					break;
+				}
+			} else {
+				int read = recv(fd, rbuf, 1024, 0);
+				if (read <= 0) {
+					remove_client(fd);
+					break;
+				}
+				rbuf[read] = '\0';
+				msg[fd] = str_join(msg[fd], rbuf);
+				deliver(fd);
+			}
+		}
+	}
+	return (0);
+}
